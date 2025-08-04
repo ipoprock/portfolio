c692754b3d241c7418e80f4ca663f730c6e0ba90
Shubham Kushwaha <poprock210496@gmail.com>
2025-08-04 13:18:49 +0530

    s

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
-  alert('Thank you! I will get back to you soon.');
+  alert('Message sent! I will reply soon.');
 });
